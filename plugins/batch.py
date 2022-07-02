7c56d8e8cd3a0eb3b79725da5823be5d08975ecf
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2022-07-02 23:22:32 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -171,7 +171,8 @@ async def cancel(c, m):
                                 print("The given link is either excluded domain link or a droplink link")
                             else:
                                 await message.edit_caption(link)
-
+                                await message.reply_photo("https://telegra.ph/file/69fed606278715d9b8077.jpg", caption=link)
+ 
                     elif METHOD == "mdlink":
 
                         # reply markup - button post
