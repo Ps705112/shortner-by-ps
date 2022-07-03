c20a9d54ef0b2fbb2c8750165d22926fbc19eed5
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2022-07-03 13:15:03 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -98,8 +98,8 @@ async def channel_link_handler(bot, message):
 					print("The given link is either excluded domain link or a droplink link")
 				else:
 					
-					await message.edit_caption(link)
-					await message.reply_photo("https://telegra.ph/file")
+					
+					await message.reply_photo("https://telegra.ph/file/69fed606278715d9b8077.jpg", caption=link)
 					await message.delete.()
 					
 					
